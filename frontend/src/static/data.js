8fd70f9b607b2d47e0c06c37c35a636f827a811b
fazalshahzad <shancompany322@gmail.com>
2023-07-14 18:35:08 +0500

    c

--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -592,56 +592,21 @@ export const navItems = [
   ];
   
   export const footerProductLinks = [
-    {
-      name: "About us",
-      link: "/about"
-    },
-    {
-      name: "Careers",
-      link: "/carrers"
-    },
+   
     {
       name: "Store Locations",
-    },
-    {
-      name: "Our Blog",
-    },
-    {
-      name: "Reviews",
     },
   ];
   
   export const footercompanyLinks = [
-    {
-      name: "Game & Video",
-    },
-    {
-      name: "Phone &Tablets",
-    },
-    {
-      name: "Computers & Laptop",
-    },
-    {
-      name: "Sport Watches",
-    },
-    {
-      name: "Events",
-    },
+    
   ];
   
   export const footerSupportLinks = [
     {
       name: "FAQ",
     },
-    {
-      name: "Reviews",
-    },
-    {
-      name: "Contact Us",
-    },
-    {
-      name: "Shipping",
-    },
+   
     {
       name: "Live chat",
     },
