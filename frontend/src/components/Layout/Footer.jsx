d89a6be62bb151e77109c1673d6deab1abcb06c5
fazalshahzad <shancompany322@gmail.com>
2023-07-14 15:28:05 +0500

    c

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -37,8 +37,10 @@ const Footer = () => {
       <div className="grid grid-cols-1 sm:gird-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16 sm:text-center">
         <ul className="px-5 text-center sm:text-start flex sm:block flex-col items-center">
           <img
-            src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+            src="https://i.ibb.co/HgTK048/Picsart-23-07-13-21-01-54-585.png"
             alt=""
+            height={50} // Add the desired height value
+            width={100} // Add the desired width value
             style={{ filter: "brightness(0) invert(1)" }}
           />
           <br />
