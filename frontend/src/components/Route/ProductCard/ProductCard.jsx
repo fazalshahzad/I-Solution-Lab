d102f73e8729fa6be70d52661974400978d25d3d
Isolutiontraders <139568173+Isolutiontraders@users.noreply.github.com>
2023-07-15 22:00:21 +0500

    c

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -89,7 +89,7 @@ const ProductCard = ({ data,isEvent }) => {
                 {data.originalPrice === 0
                   ? data.originalPrice
                   : data.discountPrice}
-                $
+                Rs
               </h5>
               <h4 className={`${styles.price}`}>
                 {data.originalPrice ? data.originalPrice + " Rs" : null}
