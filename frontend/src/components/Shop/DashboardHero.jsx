8e244701b945912ad60072202209cc663e850b5d
Isolutiontraders <139568173+Isolutiontraders@users.noreply.github.com>
2023-07-17 17:54:50 +0500

    c

--- frontend/src/components/Shop/DashboardHero.jsx
+++ frontend/src/components/Shop/DashboardHero.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineArrowRight, AiOutlineMoneyCollect } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { MdBorderClear } from "react-icons/md";
@@ -16,14 +16,15 @@ const DashboardHero = () => {
   const { products } = useSelector((state) => state.products);
 
   useEffect(() => {
-    dispatch(getAllOrdersOfShop(seller._id));
-    dispatch(getAllProductsShop(seller._id));
+     dispatch(getAllOrdersOfShop(seller._id));
+     dispatch(getAllProductsShop(seller._id));
   }, [dispatch]);
 
   const availableBalance = seller?.availableBalance.toFixed(2);
 
   const columns = [
     { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
     {
       field: "status",
       headerName: "Status",
@@ -42,6 +43,7 @@ const DashboardHero = () => {
       minWidth: 130,
       flex: 0.7,
     },
+
     {
       field: "total",
       headerName: "Total",
@@ -49,6 +51,7 @@ const DashboardHero = () => {
       minWidth: 130,
       flex: 0.8,
     },
+
     {
       field: " ",
       flex: 1,
@@ -72,20 +75,38 @@ const DashboardHero = () => {
 
   const row = [];
 
-  orders &&
-    orders.forEach((item) => {
-      row.push({
+  orders && orders.forEach((item) => {
+    row.push({
         id: item._id,
         itemsQty: item.cart.reduce((acc, item) => acc + item.qty, 0),
         total: "Rs " + item.totalPrice,
         status: item.status,
       });
-    });
-
+  });
   return (
     <div className="w-full p-8">
       <h3 className="text-[22px] font-Poppins pb-2">Overview</h3>
       <div className="w-full block 800px:flex items-center justify-between">
+        <div className="w-full mb-4 800px:w-[30%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
+          <div className="flex items-center">
+            <AiOutlineMoneyCollect
+              size={30}
+              className="mr-2"
+              fill="#00000085"
+            />
+            <h3
+              className={`${styles.productTitle} !text-[18px] leading-5 !font-[400] text-[#00000085]`}
+            >
+              Account Balance{" "}
+              <span className="text-[16px]">(with 10% service charge)</span>
+            </h3>
+          </div>
+          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">Rs{availableBalance}</h5>
+          <Link to="/dashboard-withdraw-money">
+            <h5 className="pt-4 pl-[2] text-[#077f9c]">Withdraw Money</h5>
+          </Link>
+        </div>
+
         <div className="w-full mb-4 800px:w-[30%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
           <div className="flex items-center">
             <MdBorderClear size={30} className="mr-2" fill="#00000085" />
@@ -95,9 +116,7 @@ const DashboardHero = () => {
               All Orders
             </h3>
           </div>
-          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">
-            {orders && orders.length}
-          </h5>
+          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{orders && orders.length}</h5>
           <Link to="/dashboard-orders">
             <h5 className="pt-4 pl-2 text-[#077f9c]">View Orders</h5>
           </Link>
@@ -116,9 +135,7 @@ const DashboardHero = () => {
               All Products
             </h3>
           </div>
-          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">
-            {products && products.length}
-          </h5>
+          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{products && products.length}</h5>
           <Link to="/dashboard-products">
             <h5 className="pt-4 pl-2 text-[#077f9c]">View Products</h5>
           </Link>
@@ -127,13 +144,13 @@ const DashboardHero = () => {
       <br />
       <h3 className="text-[22px] font-Poppins pb-2">Latest Orders</h3>
       <div className="w-full min-h-[45vh] bg-white rounded">
-        <DataGrid
-          rows={row}
-          columns={columns}
-          pageSize={10}
-          disableSelectionOnClick
-          autoHeight
-        />
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSize={10}
+        disableSelectionOnClick
+        autoHeight
+      />
       </div>
     </div>
   );
