f6cad508039411ecfdaefbc557f2fe655110ab92
Isolutiontraders <139568173+Isolutiontraders@users.noreply.github.com>
2023-07-19 22:15:53 +0500

    c

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async'; // Import Helmet
-import Header from "../components/Layout/Header";
-import Hero from "../components/Route/Hero/Hero";
-import Categories from "../components/Route/Categories/Categories";
-import BestDeals from "../components/Route/BestDeals/BestDeals";
-import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
-import Events from "../components/Events/Events";
-import Sponsored from "../components/Route/Sponsored";
-import Footer from "../components/Layout/Footer";
+import Header from '../components/Layout/Header';
+import Hero from '../components/Route/Hero/Hero';
+import Categories from '../components/Route/Categories/Categories';
+import BestDeals from '../components/Route/BestDeals/BestDeals';
+import FeaturedProduct from '../components/Route/FeaturedProduct/FeaturedProduct';
+import Events from '../components/Events/Events';
+import Sponsored from '../components/Route/Sponsored';
+import Footer from '../components/Layout/Footer';
 
 const HomePage = () => {
   return (
     <div>
       <Helmet>
-        <title>Home Page</title>
+        <title>Home Page | iSolution</title>
         <meta
           name="description"
-          content="Welcome to our online store. Browse our categories, check out the best deals, and find featured products."
+          content="Welcome to iSolution's online store. Browse our categories, check out the best deals, and find featured products."
         />
+        <meta name="keywords" content="iSolution, iSolutionLab, iSolutionTraders, online store, products, deals, featured products" />
         {/* Add other meta tags as needed */}
       </Helmet>
       <Header activeHeading={1} />
