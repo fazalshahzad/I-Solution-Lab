9aadeaaaae016213c581b56f64f216738d3d6097
Isolutiontraders <139568173+Isolutiontraders@users.noreply.github.com>
2023-07-19 21:37:55 +0500

    c

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import Store from "./redux/store";
 
 ReactDOM.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <HelmetProvider>
+      <Provider store={Store}>
+        {/* Your main application component */}
+        <App />
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
