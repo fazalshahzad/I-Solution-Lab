2552dff54ac550f711a438b4909ccb1be3024269
Isolutiontraders <139568173+Isolutiontraders@users.noreply.github.com>
2023-07-22 16:22:50 +0500

    c

--- frontend/src/components/Route/Sponsored.jsx
+++ frontend/src/components/Route/Sponsored.jsx
@@ -3,6 +3,9 @@ import styles from "../../styles/styles";
 
 const Sponsored = () => {
   return (
+
+
+    
     <div
       className={`${styles.section} hidden sm:block bg-white py-10 px-5 mb-12 cursor-pointer rounded-xl`}
     >
